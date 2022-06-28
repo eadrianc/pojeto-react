8e80c39bfaa2b3fdcf12e5959c63af085111c199
eadrianc <102111847+eadrianc@users.noreply.github.com>
2022-06-28 00:32:52 -0300

    terceiro commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,38 @@ import React, { useEffect, useState } from "react";
 
 function Registers({ list = [] }){
 
+    const [order, setOrder] = useState(1)
+    const [colunmOrder, setColunmOrder] = useState('nome')
+    const [filter, setfilter] = useState ('')
 
-    return <table>
+    const handleOrder = fieldName => {
+        setOrder(-order)
+        setColunmOrder(fieldName)
+    }
+
+    list = list.sort((a, b) => {
+        return a[colunmOrder] < b[colunmOrder] ? -order : order ;
+    })
+
+    if(filter)
+    list = list.filter( item =>
+        item.nome.tolowerCase().indexOf(filter.tolowerCase())
+        )
+
+        const handleFilter = e => {
+             setfilter( e.target.value)
+        }
+
+
+    return <div>
+        <input placeholder='Pesquisar' onChange={handleFilter} />
+     <table>
         <thead>
             <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Telefone</th>
+                <th onClick={e => handleOrder('id')}>Id</th>
+                <th onClick={e => handleOrder('nome')}>Nome</th>
+                <th onClick={e => handleOrder('email')}>Email</th>
+                <th onClick={e => handleOrder('telefone')}>Telefone</th>
             </tr>
 
         </thead>
@@ -33,6 +57,7 @@ function Registers({ list = [] }){
            }
         </tbody>
        </table>
+       </div>
 
 
 }
@@ -44,10 +69,13 @@ function App() {
 
         useEffect( () => {
 
-            fetch('http://localhost:3000/contatos')
-            .then (async result => {
-                setList(await result.json() )
+            
+            fetch('http://localhost:3001/contatos')
+            .then(async result => {
+            setList( await result.json() )
+
             })
+            
 
         }, [] )
 
